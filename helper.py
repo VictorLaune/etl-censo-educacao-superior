5009c87749b8591408f76e01ce4e77d1bfb17a7c
Victor Laune <laune.vh@gmail.com>
2023-04-03 01:19:31 -0300

    conclusao do download e conversao dos dados para parquet

--- helper.py
+++ helper.py
@@ -1,8 +1,17 @@
-import os
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as f
 from zipfile import ZipFile
+import os
 import requests
 import os
 import io
+
+# Criando o SparkSession
+spark = SparkSession\
+   .builder\
+   .master("local")\
+   .appName("convert_to_parquet")\
+   .getOrCreate()
 
 # Automatiza o download dos arquivos
 def baixando_e_extraindo_arquivos(anos_para_download):
@@ -35,3 +44,29 @@ def verifica_pasta(nome_da_pasta):
         except:
             print("Error!")
     return path
+
+def conversao_parquet_csv_sep_ponto_e_virgula(path, arquivo, pasta):
+    params = {'header':True, 'inferSchema':True, 'sep':';'}
+    path_csv = path + '/' + arquivo
+    df = (
+        spark
+        .read
+        .csv(path_csv, **params)
+        )
+
+    df = df.select([f.col(coluna).alias(coluna.lower()) for coluna in df.columns])
+
+    df.write.parquet(f"to_parquet/{pasta}/{arquivo}", mode='overwrite')
+
+def conversao_parquet_csv_sep_barra(path, arquivo, pasta):
+    params = {'header':True, 'inferSchema':True, 'sep':'|'}
+    path_csv = path + '/' + arquivo
+    df = (
+        spark
+        .read
+        .csv(path_csv, **params)
+        )
+
+    df = df.select([f.col(coluna).alias(coluna.lower()) for coluna in df.columns])
+
+    df.write.parquet(f"to_parquet/{pasta}/{arquivo}", mode='overwrite')
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from helper import *
+import shutil
 
 # Definindo os anos que serao selecionados para download
 anos_para_download = []
 ano = 2021
-ano_final = 2021
+ano_final = 1995
+
 
 while ano >= ano_final:
     anos_para_download.append(str(ano))
@@ -13,7 +15,27 @@ print("Anos que iremos utilizar para a extração:" + str(anos_para_download) + 
 
 baixando_e_extraindo_arquivos(anos_para_download)
 
-
+# Criando pasta data, renomeando arquivos não utf-8 e removendo pasta download
 os.mkdir("./data")
 renomeando_pastas()
 os.rmdir('./download')
+
+# Listando pastas fazermos a verificacao 
+lista_pastas = ([pasta for pasta in os.listdir("./data")])
+
+
+for pasta in lista_pastas:
+    path = verifica_pasta(pasta)
+    lista_arquivos = ([arquivo for arquivo in os.listdir(f"{path}")])
+    for arquivo in lista_arquivos:
+        if(arquivo[-3:] == 'CSV' or arquivo[-3:] == 'csv'):
+            try: 
+                conversao_parquet_csv_sep_ponto_e_virgula(path, arquivo, pasta)
+            except:
+                print("É preciso utilizar ''|' como separador")
+                try:
+                    conversao_parquet_csv_sep_barra(path, arquivo, pasta)                    
+                except:
+                    print('Error!')
+
+shutil.rmtree('./data')
